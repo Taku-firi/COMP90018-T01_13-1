6adedd31c4a1d3b948e09e16af99a5a36af68854
Takumichan <37031501+Taku-firi@users.noreply.github.com>
2022-10-03 20:18:15 +1100

    Implement Google Map
    Implement Google Map in the EventsFragment

--- app/src/main/java/com/comp90018/assignment2/application/ui/events/EventsFragment.java
+++ app/src/main/java/com/comp90018/assignment2/application/ui/events/EventsFragment.java
@@ -12,11 +12,19 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.comp90018.assignment2.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class EventsFragment extends Fragment {
+public class EventsFragment extends Fragment implements OnMapReadyCallback{
 
     private EventsViewModel eventsViewModel;
+    GoogleMap map;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,8 +33,19 @@ public class EventsFragment extends Fragment {
                 new ViewModelProvider(this).get(EventsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_events, container, false);
 
+        SupportMapFragment mapFragment =
+                (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        map = googleMap;
+        LatLng location = new LatLng(-37.7963,144.9614);
+        map.addMarker(new MarkerOptions().position(location).title("You are here"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(location));
     }
 }
