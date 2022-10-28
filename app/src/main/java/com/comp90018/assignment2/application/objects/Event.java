a697867f436d1d02a05c456389372f0b4f0d85c7
Takumichan <37031501+Taku-firi@users.noreply.github.com>
2022-10-28 19:00:03 +1100

    Add two objects
    “Event" and "User"
    
    Co-Authored-By: DAyii99 <43560698+DAyii99@users.noreply.github.com>
    Co-Authored-By: jxz2021114 <49898067+jxz2021114@users.noreply.github.com>

---
+++ app/src/main/java/com/comp90018/assignment2/application/objects/Event.java
@@ -0,0 +1,66 @@
+package com.comp90018.assignment2.application.objects;
+
+public class Event {
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private String type;
+    private String date;
+    private String detail;
+
+    public Event(){}
+
+    public Event(String name, Double latitude,Double longitude){
+        this.name=name;
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
---
+++ app/src/main/java/com/comp90018/assignment2/application/objects/User.java
@@ -0,0 +1,86 @@
+package com.comp90018.assignment2.application.objects;
+
+import java.util.ArrayList;
+
+public class User {
+    private String uid;
+    private String name;
+    private String email;
+    private String gender;
+    private int age;
+    private String interest;
+    private String detail;
+    private ArrayList<Event> events;
+
+    public User(){}
+
+    public User(String name,String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
+    }
+}
+
