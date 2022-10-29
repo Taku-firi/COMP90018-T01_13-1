18d648e4b88b5b25072d1cc1249f270020a307f7
unknown <jxz2021114@gmail.com>
2022-10-29 23:23:07 +1100

    git commit app/src/main/java/com/comp90018/assignment2/application/ui/profile/ProfileFragment.java app/src/main/res/layout/frag ment_profile.xml

--- app/src/main/java/com/comp90018/assignment2/application/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/comp90018/assignment2/application/ui/profile/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.comp90018.assignment2.application.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +17,13 @@ import com.comp90018.assignment2.R;
 
 public class ProfileFragment extends Fragment {
     private ProfileViewModel profileViewModel;
+    private TextView profileUserName;
+    private TextView profileUserId;
+    private TextView profileGender;
+    private TextView profileAge;
+    private TextView profileLocation;
+    private TextView profileInterests;
+    private TextView profileSelfIntroduction;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -22,8 +32,33 @@ public class ProfileFragment extends Fragment {
                 new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        profileUserName= root.findViewById(R.id.profileUserName);
+        profileUserId= root.findViewById(R.id.profileUserId);
+        profileGender= root.findViewById(R.id.profileGender);
+        profileAge= root.findViewById(R.id.profileAge);
+        profileLocation= root.findViewById(R.id.profileLocation);
+        profileInterests= root.findViewById(R.id.profileInterests);
+        profileSelfIntroduction= root.findViewById(R.id.profileSelfIntroduction);
 
+        //pressure.setText(String.valueOf(event.getPressure()));
+        profileUserName.setText("User name: "+"Xiao Ming");
+        profileUserId.setText("User Id: "+"001");
+        profileGender.setText("Gender: "+"Male");
+        profileAge.setText("Age: "+"22");
+        profileLocation.setText("Location: "+"Melbourne");
+        profileInterests.setText("Interests: "+"Swimming");
+        profileSelfIntroduction.setText("Self introduction: "+"Love sports.");
+
+        Button button= root.findViewById(R.id.profileSettigs);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ProfileFragment.this.getActivity(), ProfileSetting.class);
+                ProfileFragment.this.startActivity(myIntent);            }
+        });
 
         return root;
     }
+
+
 }
